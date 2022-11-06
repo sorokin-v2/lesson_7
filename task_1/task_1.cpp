b91cf35037e343a4696f38816ed4beb11e0ca094
Valery Sorokin <podrabotka-2019@yandex.ru>
2022-11-06 17:46:15 +0300

    first commit

---
+++ task_1/task_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include<vector>
+#include<set>
+#include <algorithm>
+
+std::vector<int> get_unique(const std::vector<int>& src) {
+    std::set<int> unique_src(src.cbegin(), src.cend());
+    return std::vector<int>(unique_src.cbegin(), unique_src.cend());
+}
+
+//Не знаю какой вариант эффективнее
+
+std::vector<int> get_unique2(std::vector<int> src) {
+    std::sort(src.begin(), src.end());
+    src.erase(std::unique(src.begin(), src.end()), src.end());
+    return src;
+}
+
+int main()
+{
+    std::vector<int> src = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    for (const auto& val : src) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    std::vector<int> out = get_unique(src);
+    for (const auto& val : out) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    out = get_unique2(src);
+    for (const auto& val : out) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ task_2/task_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template<typename T>
+void print_container(const T& src) {
+    for (auto& val : src) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two. помните почему такой порядок? :)
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+}
+
